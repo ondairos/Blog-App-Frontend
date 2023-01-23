7d05b5bef6fb05833a0e8ad6164f98ef9939983a
Ondairos <ondairos@gmail.com>
2023-01-23 11:58:05 +0200

    added login service

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import { useState, useEffect } from 'react'
+
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  // handle login logic
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      // send the user token for Authorization in the backend through notesService
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 500)
+    }
+  }
 
   return (
     <div>
