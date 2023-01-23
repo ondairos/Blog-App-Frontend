1f40c6f2fda1a88ceb5b55021a3d893c0f9cdcb0
Ondairos <ondairos@gmail.com>
2023-01-23 12:02:21 +0200

    added Notification error component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 
 import Blog from './components/Blog'
-import blogService from './services/blogs'
+import Notification from './components/Notification'
 
+import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -41,7 +44,10 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
+
+      <Notification message={errorMessage} />
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
