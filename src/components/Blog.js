1a93108f333388ed937090961449b9893044845d
Ondairos <ondairos@gmail.com>
2023-01-24 16:53:39 +0200

    changes to Blog component(css)

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,14 +2,21 @@ import { useState } from "react"
 import Togglable from "./Togglable"
 
 const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
 
   return (
-    <div>
+    <div style={blogStyle}>
       ||{blog.title}||  by: {blog.author}
       <Togglable buttonLabel ='blog_view'>
         {blog.url}
         <br></br>
-        {blog.likes}     
+       {blog.likes ? blog.likes : 0}     
         <br></br>
         {blog.user.name}     
         <br></br>
