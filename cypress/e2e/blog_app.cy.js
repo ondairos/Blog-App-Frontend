d8706248a0fee0abb2115def6eaac156339fb228
Ondairos <ondairos@gmail.com>
2023-01-27 16:14:24 +0200

    added more login tests and cypress_plugin

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,14 +1,20 @@
-/* eslint-disable no-undef */
 // Mocha recommends that arrow functions are not used, because they might cause some issues in certain situations.
 describe('Blog app', function () {
-    it('front page can be opened', function () {
+    beforeEach(function() {
         cy.visit('http://localhost:3000')
+    })
+
+    it('front page can be opened', function () {
         cy.contains('Blogs')
         cy.contains('Ioannis Kantiloros')
     })
 
     it('login form can be opened', function () {
-        cy.visit('http://localhost:3000')
         cy.contains('login').click()
+        cy.get('#username').type('test2')
+        cy.get('#password').type('13141')
+        cy.get('#login-button').click()
+
+        cy.contains('Testing Test2 logged-in')
     })
 })
