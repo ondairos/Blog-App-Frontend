367146dc30656185f3df84df22b39ce7cb3bbca2
Ondairos <ondairos@gmail.com>
2023-02-13 19:59:58 +0200

    eslint fixes

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -6,28 +6,28 @@ let token = null
 const setToken = newToken => { token = `bearer ${newToken}` }
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
 // create function for blog service
 const create = async newBlogObject => {
-  const headerConfiguration = { headers: { Authorization: token }, }
-  const response = await axios.post(baseUrl, newBlogObject, headerConfiguration)
-  return response.data
+    const headerConfiguration = { headers: { Authorization: token }, }
+    const response = await axios.post(baseUrl, newBlogObject, headerConfiguration)
+    return response.data
 }
 
 //put function
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
 }
 
 //delete
 const deleteB = async (id) => {
-  const headerConfiguration = { headers: { Authorization: token }, }
-  const request = await axios.delete(`${baseUrl}/${id}`, headerConfiguration)
-  return request.data
+    const headerConfiguration = { headers: { Authorization: token }, }
+    const request = await axios.delete(`${baseUrl}/${id}`, headerConfiguration)
+    return request.data
 }
 
 const exportBlogsService = { getAll, create, update, deleteB, setToken }
