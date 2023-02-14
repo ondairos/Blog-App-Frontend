1596ecc562291462d2f31d4d2f9f67cf2f2614d2
Ondairos <ondairos@gmail.com>
2023-02-14 15:45:11 +0200

    added notification reducer and in store.js

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import reducer from './reducers/blogReducer'
 import { configureStore } from '@reduxjs/toolkit'
+import notificationReducer from './reducers/notificationReducer'
 
 // config reduxjs/toolkit
 const store = configureStore({
     reducer: {
         blogs: reducer,
-        // notification: notificationReducer
+        notification: notificationReducer
     }
 })
 
